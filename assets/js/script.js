76e7e77c9a57703dfb12104910e994a6690354c2
Jonathanrange <jonathan.range@outlook.com>
2021-11-09 14:21:58 +0000

    Add array of questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,62 @@
-console.log('test');
+let questions = [
+    {
+        question: 'Which band is often cited as being one of the founders of heavy metal?',
+        answers: {
+            A: 'The Who',
+            B: 'ABBA',
+            C: 'Black Sabbath',
+            D: 'Pink Floyd'
+        },
+        correctAnswer: 'C' 
+    },
+    {
+        question: 'Finish the following lyrics: "We will, we will ..."',
+        answers: {
+            A: 'Give you a haircut',
+            B: 'Rock you',
+            C: 'Eat dinner',
+            D: 'Punch you in the face'
+        },
+        correctAnswer: 'B'
+    },
+    {
+        question: 'Rock and metal music is often characterized by the use of distortion on a particular instrument. Which one?',
+        answers: {
+            A: 'Guitar',
+            B: 'Trombone',
+            C: 'Piano',
+            D: 'Bass'
+        },
+        correctAnswer: 'A' 
+    },
+    {
+        question: 'In 1969, Jimi Hendrix played at one of the most legendary music festivals of all time. What was it called?',
+        answers: {
+            A: 'Flower Power',
+            B: 'Lollapalooza',
+            C: 'Woodstock',
+            D: 'Ragnarök'
+        },
+        correctAnswer: 'C' 
+    },
+    {
+        question: 'Metal shows are infamous for their crazy crowds, with people running into each other as a sort of ritual. This is often referred to as a ...',
+        answers: {
+            A: 'Dance',
+            B: 'Way to get to know each other',
+            C: 'Party',
+            D: 'Moshpit'
+        },
+        correctAnswer: 'D' 
+    },
+    {
+        question: 'What is, without a doubt, the best music genre in the world?',
+        answers: {
+            A: 'Metal',
+            B: 'Metal',
+            C: 'Metal',
+            D: 'Metal (Preferably LOUD)'
+        },
+        correctAnswer: 'A, B, C, D'
+    }
+]
